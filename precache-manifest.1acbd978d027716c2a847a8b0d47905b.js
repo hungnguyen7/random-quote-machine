e2a2f5bf64c82e0b79204022555c6ff6f2e0107f
hungnguyen7 <hungnguyenst.99@gmail.com>
2020-05-14 16:18:15 +0700

    Updates

---
+++ precache-manifest.1acbd978d027716c2a847a8b0d47905b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d162433139ede9bc6c06755aa7d75cc",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "e5546368a1586835b89b",
+    "url": "/random-quote-machine/static/css/main.f3b4af2d.chunk.css"
+  },
+  {
+    "revision": "6224b2032766df697050",
+    "url": "/random-quote-machine/static/js/2.6fe3310c.chunk.js"
+  },
+  {
+    "revision": "8405fc7db03aa55859ceaaa8183fa901",
+    "url": "/random-quote-machine/static/js/2.6fe3310c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5546368a1586835b89b",
+    "url": "/random-quote-machine/static/js/main.0bd7a406.chunk.js"
+  },
+  {
+    "revision": "5d10ac826fadbd9fa6e6",
+    "url": "/random-quote-machine/static/js/runtime-main.62554c6d.js"
+  }
+]);
